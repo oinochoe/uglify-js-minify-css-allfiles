85ed46f945c9950741b3af1050be0940c45e6d47
Yeongmin Kim <oinochoe@naver.com>
2024-08-19 22:46:26 +0900

    ✨ Feat: Apply Babel conversion options.

--- demo.js
+++ demo.js
@@ -1,5 +1,16 @@
 import minifyAll from './dist/module.js';
 
-minifyAll('./test/', 'lib');
+minifyAll('./test/', 'lib', {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    esmodules: false, // Target ES2015
+                },
+            },
+        ],
+    ],
+});
 // console.log(process.argv.slice(2)[0], process.argv.slice(2)[1]);
 // minifyAll(process.argv.slice(2)[0], process.argv.slice(2)[1]);
--- dist/module.js
+++ dist/module.js
@@ -10,11 +10,24 @@ import path from 'path';
 import Logger from './modules/logger.js';
 import { getAllFiles, writeFile } from './modules/fileHandler.js';
 import { minifyJS, minifyCSS } from './modules/minifier.js';
+import babelCore from '@babel/core';
 
 const FILE_HANDLERS = {
-    '.js': async (filePath, content, logger) => {
-        const result = minifyJS(content);
-        await writeFile(filePath, result, logger);
+    '.js': async (filePath, content, logger, babelOptions) => {
+        try {
+            // Transform the code using Babel with preset-env
+            let transformed = content;
+
+            // Convert code to Babel when Babel option is provided
+            if (babelOptions) {
+                transformed = babelCore.transformSync(content, babelOptions).code;
+            }
+
+            const result = minifyJS(transformed);
+            await writeFile(filePath, result, logger);
+        } catch (error) {
+            await logger.logError(filePath, `Babel transformation failed: ${error.message}`);
+        }
     },
     '.css': async (filePath, content, logger) => {
         const output = await minifyCSS(content);
@@ -32,17 +45,18 @@ const FILE_HANDLERS = {
  * @async
  * @param {string} filePath - The path of the file to process.
  * @param {Logger} logger - The logger instance.
+ * @param {Object} [babelOptions=null] - Babel options for converting JavaScript files.
  * @returns {Promise<void>}
  */
-async function processFile(filePath, logger) {
+async function processFile(filePath, logger, babelOptions = null) {
     const fileContent = await fs.readFile(filePath, 'utf-8');
     const fileExtension = path.extname(filePath);
     const handler = FILE_HANDLERS[fileExtension];
 
     if (handler) {
-        await handler(filePath, fileContent, logger);
+        await handler(filePath, fileContent, logger, babelOptions);
     } else {
-        logger.logInfo(`Skipping unsupported file type: ${filePath}`);
+        logger.logInfo(`지원되지 않는 파일 형식 건너뜀: ${filePath}`);
     }
 }
 
@@ -53,6 +67,7 @@ async function processFile(filePath, logger) {
  * @function minifyAll
  * @param {string} contentPath - The path to the directory containing the files to be minified.
  * @param {string} [excludeFolder=''] - The name of a folder to exclude from minification.
+ * @param {Object} [babelOptions=null] - Babel options for converting JavaScript files.
  * @returns {Promise<void>} A promise that resolves when all files have been processed.
  * @throws {Error} If there's an issue reading or writing files.
  *
@@ -65,8 +80,23 @@ async function processFile(filePath, logger) {
  * // Minify all files in a directory specified as a command line argument
  * import minifyAll from './main.js';
  * await minifyAll(process.argv[2], process.argv[3]);
+ *
+ * @example
+ * // Add babel options for converting JavaScript files.
+ * minifyAll('./test/', 'lib', {
+ *     presets: [
+ *         [
+ *             '@babel/preset-env',
+ *             {
+ *                 targets: {
+ *                     esmodules: false, // Target ES2015
+ *                 },
+ *             },
+ *         ],
+ *     ],
+ * });
  */
-export default async function minifyAll(contentPath, excludeFolder = '') {
+export default async function minifyAll(contentPath, excludeFolder = '', babelOptions = null) {
     const logger = new Logger();
     await logger.initialize();
 
@@ -74,7 +104,7 @@ export default async function minifyAll(contentPath, excludeFolder = '') {
 
     await getAllFiles(rootDir, async (filePath) => {
         if (excludeFolder && filePath.includes(excludeFolder)) return;
-        await processFile(filePath, logger);
+        await processFile(filePath, logger, babelOptions);
     });
 
     await logger.summarize();
